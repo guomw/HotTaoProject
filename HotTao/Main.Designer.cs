87e4dc2ba51865c61946f3f20e225d6d1f0bc578
unknown <>
2017-02-18 19:40:04 +0800

    create project

---
+++ HotTao/Main.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace HotTao
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Container = new System.Windows.Forms.SplitContainer();
+            this.btnHome = new System.Windows.Forms.PictureBox();
+            this.btnTimer = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Container)).BeginInit();
+            this.Container.Panel1.SuspendLayout();
+            this.Container.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnHome)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnTimer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Container
+            // 
+            this.Container.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Container.Location = new System.Drawing.Point(0, 0);
+            this.Container.Name = "Container";
+            this.Container.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // Container.Panel1
+            // 
+            this.Container.Panel1.AutoScroll = true;
+            this.Container.Panel1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Container.Panel1.Controls.Add(this.pictureBox5);
+            this.Container.Panel1.Controls.Add(this.pictureBox4);
+            this.Container.Panel1.Controls.Add(this.pictureBox3);
+            this.Container.Panel1.Controls.Add(this.btnTimer);
+            this.Container.Panel1.Controls.Add(this.btnHome);
+            this.Container.Size = new System.Drawing.Size(930, 652);
+            this.Container.SplitterDistance = 149;
+            this.Container.TabIndex = 0;
+            // 
+            // btnHome
+            // 
+            this.btnHome.Image = global::HotTao.Properties.Resources.home;
+            this.btnHome.Location = new System.Drawing.Point(29, 29);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(74, 74);
+            this.btnHome.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnHome.TabIndex = 0;
+            this.btnHome.TabStop = false;
+            // 
+            // btnTimer
+            // 
+            this.btnTimer.Image = global::HotTao.Properties.Resources.home;
+            this.btnTimer.Location = new System.Drawing.Point(131, 29);
+            this.btnTimer.Name = "btnTimer";
+            this.btnTimer.Size = new System.Drawing.Size(74, 74);
+            this.btnTimer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnTimer.TabIndex = 0;
+            this.btnTimer.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::HotTao.Properties.Resources.home;
+            this.pictureBox3.Location = new System.Drawing.Point(238, 29);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(74, 74);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::HotTao.Properties.Resources.home;
+            this.pictureBox4.Location = new System.Drawing.Point(336, 29);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(74, 74);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 0;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::HotTao.Properties.Resources.home;
+            this.pictureBox5.Location = new System.Drawing.Point(440, 29);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(74, 74);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 0;
+            this.pictureBox5.TabStop = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 652);
+            this.Controls.Add(this.Container);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main";
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.Container.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Container)).EndInit();
+            this.Container.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnHome)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnTimer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer Container;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox btnTimer;
+        private System.Windows.Forms.PictureBox btnHome;
+    }
+}
---
+++ HotTao/Main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HotTao
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
